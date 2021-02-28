52efac5b513ff1548e54a48b9a604fd54aaaf836
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2021-02-28 23:37:48 +0700

    assignment11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { lab4Component } from './lab4Component/lab4.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    lab4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/lab4Component/lab4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component ({
+ selector: "app-lab4",
+ templateUrl: "./lab4.component.html",
+ styleUrls: ["./lab4.component.css"]
+})
+
+export class lab4Component {
+ arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+ constructor() {}
+}
